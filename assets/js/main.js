c95cf9680a85e83d91bc1f41dfc1ac2fba2327b1
ChiaraPapagni <chiaraiors@gmail.com>
2021-10-14 15:18:04 +0200

    aggiunta logica gioco - modificato stile .form_play

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,37 @@ cancelButtonElement.addEventListener('click', function () {
     message.innerHTML = '';
 
 });
+
+
+/* Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto */
+
+//seleziono elementi dalla DOM
+const playlButtonElement = document.getElementById('play');
+const userNumberRandomElement = document.getElementById('user-number');
+const pcNumberRandomElement = document.getElementById('pc-number');
+
+playlButtonElement.addEventListener('click', function () {
+
+    //genero numero random per pc e giocatore
+    let pcNumber = Math.floor((Math.random() * 6) + 1);
+    let userNumber = Math.floor((Math.random() * 6) + 1);
+
+    //stampo in pagina i numeri generati
+    document.getElementById('pc-number').innerHTML = pcNumber;
+    document.getElementById('user-number').innerHTML = userNumber;
+
+    let message;
+
+    //confronto chi fa il punteggio più alto
+    if (pcNumber > userNumber) {
+        message = `Vince il computer!`;
+    } else if (pcNumber < userNumber) {
+        message = `Vince il giocatore!`;
+    } else {
+        message = `Pareggio`;
+    }
+
+    //stampo in pagina il vincitore
+    document.getElementById('winner').innerHTML = message;
+});
